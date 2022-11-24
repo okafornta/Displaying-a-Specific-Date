70a976cf2b4bb9abde8cf5804608c712777a1aad
Okafornta <hillaryonia@gmail.com>
2022-11-24 10:46:00 +0100

    Add project files.

---
+++ Displaying a Specific Date/Program.cs
@@ -0,0 +1,11 @@
+﻿/* TASK:
+ *  When working with dates, you do not have to always start from today’s date. You can
+    choose some specific date.
+ */
+DateTime newFormat = new DateTime(1992, 9, 29);
+
+// output 
+Console.WriteLine (" I was born on " + newFormat.ToLongDateString());
+
+// Waiting for Enter
+Console.ReadLine();
